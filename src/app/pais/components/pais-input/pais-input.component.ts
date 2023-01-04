0d4af0624c05c399a5c3ca694596ca996ca08b2c
Anderson cardenas <andyguaca@gmail.com>
2023-01-04 17:29:11 -0500

    this is a web page that use a API to search Countries and capitals to show in tables

---
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { debounce, debounceTime, Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html',
+  styles: [
+  ]
+})
+export class PaisInputComponent implements OnInit {
+
+  @Output() onEnter:EventEmitter<string> = new EventEmitter();
+  //este debounce funciona cuando deja de escribir y manda la cadena como este
+  @Output() onDebounce:EventEmitter<string> = new EventEmitter();
+
+  debouncer:Subject<string> = new Subject();
+
+  termino:string ='';
+
+
+  ngOnInit(): void {
+      this.debouncer.pipe(debounceTime(300))//emita el valor cada 300 mms
+      .subscribe(val =>{
+          this.onDebounce.emit(val);
+          console.log(val)
+      })
+  }
+
+  buscar(){
+    this.onEnter.emit(this.termino);
+  }
+
+  teclaPrecionada(){
+    this.debouncer.next( this.termino);
+  }
+}
