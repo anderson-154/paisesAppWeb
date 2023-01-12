955a812cf88364ceb667593eda9eee0dae26eee8
Anderson cardenas <andyguaca@gmail.com>
2023-01-12 15:50:50 -0500

    suggestion dropdown bar with debouncer

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,11 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+    li{
+      cursor: pointer
+    }
+    `
   ]
 })
 export class PorPaisComponent  {
@@ -13,7 +18,7 @@ export class PorPaisComponent  {
   termino:string = '';
   hayError:boolean=false;
   paises: Country[]=[];
-
+  paisesSugeridos: Country[]=[];
 
   constructor(private paisService:PaisService) { }
 
@@ -34,6 +39,10 @@ export class PorPaisComponent  {
   }
 
   sugerencias(event:string){
-
+    this.hayError = false;
+    this.paisService.buscarPais(event)
+      .subscribe({next:(paises)=>{
+        this.paisesSugeridos = paises.splice(0,5);
+      },error:(err) => this.paisesSugeridos = []});
   }
 }
